4f0877591773621bb03d012ed5575db85dba2fbb
PabloTsvetkov <pcvetkov53@gmail.com>
2024-10-22 23:32:25 +0300

    change logic of state to store. Now store is having everything

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,117 +1,146 @@
-let RenderEntireTree = () => {};
+let RenderEntireTree = () => { };
 
-let state = {
-    dialogsBlock: {
-        dialogsPreview:
-            [
-                { name: 'Alice Alisova', id: '1' },
-                { name: 'Sam Araratov', id: '2' },
-                { name: 'Pokrov Smallov', id: '3' },
-                { name: 'Brat Lublin', id: '4' },
-                { name: 'Red Boolev', id: '5' }
-            ],
-        dialogs: {
-            dialog1: [
+let store = {
+    _state: {
+        dialogsBlock: {
+            dialogsPreview:
+                [
+                    { name: 'Alice Alisova', id: '1' },
+                    { name: 'Sam Araratov', id: '2' },
+                    { name: 'Pokrov Smallov', id: '3' },
+                    { name: 'Brat Lublin', id: '4' },
+                    { name: 'Red Boolev', id: '5' }
+                ],
+            dialogs: {
+                dialog1: [
 
-            ],
-            dialog2: [
+                ],
+                dialog2: [
 
-            ]
+                ]
+            }
+        },
+        postsBlock: {
+            posts:
+                [
+                    {
+                        id: 1,
+                        postText: "Here is some post",
+                        likesCount: 12,
+                        commentsBlock: {
+                            commentsCount: 3,
+                            comments: [
+                                {
+                                    id: 1,
+                                    commentText: "Interesting post!"
+                                },
+                                {
+                                    id: 2,
+                                    commentText: "Yeah Yeah Yeah"
+                                },
+                                {
+                                    id: 3,
+                                    commentText: "Scrjet"
+                                }
+                            ]
+                        },
+                        repostsCount: 3,
+                        viewsCount: 132
+                    },
+                    {
+                        id: 2,
+                        postText: "Here is another post",
+                        likesCount: 25,
+                        commentsBlock: {
+                            commentsCount: 2,
+                            comments: [
+                                {
+                                    id: 1,
+                                    commentText: "Interesting post!"
+                                },
+                                {
+                                    id: 2,
+                                    commentText: "Yeah Yeah Yeah"
+                                }
+                            ]
+                        },
+                        repostsCount: 5,
+                        viewsCount: 210
+                    },
+                    {
+                        id: 3,
+                        postText: "Amazing post",
+                        likesCount: 47,
+                        commentsBlock: {
+                            commentsCount: 1,
+                            comments: [
+                                {
+                                    id: 3,
+                                    commentText: "Great content, keep it up!"
+                                }
+                            ]
+                        },
+                        repostsCount: 8,
+                        viewsCount: 350
+                    }
+                ],
+            NewPostText: 'example of text for the new post'
         }
     },
-    postsBlock: {
-        posts:
-            [
-                {
-                    id: 1,
-                    postText: "Here is some post",
-                    likesCount: 12,
-                    commentsBlock: {
-                        commentsCount: 3,
-                        comments: [
-                            {
-                                id: 1,
-                                commentText: "Interesting post!"
-                            },
-                            {
-                                id: 2,
-                                commentText: "Yeah Yeah Yeah"
-                            },
-                            {
-                                id: 3,
-                                commentText: "Scrjet"
-                            }
-                        ]
-                    },
-                    repostsCount: 3,
-                    viewsCount: 132
-                },
-                {
-                    id: 2,
-                    postText: "Here is another post",
-                    likesCount: 25,
-                    commentsBlock: {
-                        commentsCount: 2,
-                        comments: [
-                            {
-                                id: 1,
-                                commentText: "Interesting post!"
-                            },
-                            {
-                                id: 2,
-                                commentText: "Yeah Yeah Yeah"
-                            }
-                        ]
-                    },
-                    repostsCount: 5,
-                    viewsCount: 210
-                },
-                {
-                    id: 3,
-                    postText: "Amazing post",
-                    likesCount: 47,
-                    commentsBlock: {
-                        commentsCount: 1,
-                        comments: [
-                            {
-                                id: 3,
-                                commentText: "Great content, keep it up!"
-                            }
-                        ]
-                    },
-                    repostsCount: 8,
-                    viewsCount: 350
-                }
-            ],
-        NewPostText: 'example of text for the new post'
+    GetState() {
+        return this._state;
+    },
+    UpdateNewPostText(newPostText) {
+        this._state.postsBlock.NewPostText = newPostText;
+        RenderEntireTree(this._state);
+    },
+    AddPost(PostText) {
+        let NewPostObject = {
+            id: 5,
+            postText: PostText,
+            likesCount: 0,
+            commentsBlock: {
+                commentsCount: 0,
+                comments: [
+                ]
+            },
+            repostsCount: 0,
+            viewsCount: 0
+        };
+        this._state.postsBlock.posts.push(NewPostObject);
+        this._state.postsBlock.NewPostText = '';
+        RenderEntireTree(this._state);
+    },
+    subscribe(observer) {
+        RenderEntireTree = observer;
     }
 }
 
-export let UpdateNewPostText = (newPostText) => {
-    state.postsBlock.NewPostText = newPostText;
-    RenderEntireTree(state);
-}
+// export let UpdateNewPostText = (newPostText) => {
+//     state.postsBlock.NewPostText = newPostText;
+//     RenderEntireTree(state);
+// }
 
-export let AddPost = (PostText) => {
-    let NewPostObject = {
-        id: 5,
-        postText: PostText,
-        likesCount: 0,
-        commentsBlock: {
-            commentsCount: 0,
-            comments: [
-            ]
-        },
-        repostsCount: 0,
-        viewsCount: 0
-    };
-    state.postsBlock.posts.push(NewPostObject);
-    state.postsBlock.NewPostText = '';
-    RenderEntireTree(state);
-}
+// export let AddPost = (PostText) => {
+//     let NewPostObject = {
+//         id: 5,
+//         postText: PostText,
+//         likesCount: 0,
+//         commentsBlock: {
+//             commentsCount: 0,
+//             comments: [
+//             ]
+//         },
+//         repostsCount: 0,
+//         viewsCount: 0
+//     };
+//     state.postsBlock.posts.push(NewPostObject);
+//     state.postsBlock.NewPostText = '';
+//     RenderEntireTree(state);
+// }
 
-export const subscribe = (observer) => {
-    RenderEntireTree = observer;
-}
+// export const subscribe = (observer) => {
+//     RenderEntireTree = observer;
+// }
 
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
-import state, { AddPost, UpdateNewPostText, subscribe } from './Redux/state';
+// import state, { AddPost, UpdateNewPostText, subscribe } from './Redux/state';
+import store from './Redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 let RenderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
-      <App state={state} AddPost={AddPost} UpdateNewPostText={UpdateNewPostText} />
+      <App state={store.GetState()} AddPost={store.AddPost.bind(store)} UpdateNewPostText={store.UpdateNewPostText.bind(store)} />
     </React.StrictMode>
   );
 }
 
-RenderEntireTree(state);
+RenderEntireTree(store.GetState());
 
-subscribe(RenderEntireTree);
+store.subscribe(RenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
